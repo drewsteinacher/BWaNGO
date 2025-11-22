5b61aa5e3e783df340f7e1535301bcf5bf1a56e9
drewsteinacher <26029945+drewsteinacher@users.noreply.github.com>
2025-11-22 11:19:08 -0500

    Add localStorage support to counter

--- BWaNGO/Program.cs
+++ BWaNGO/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
@@ -8,6 +9,8 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddMudServices();
+builder.Services.AddBlazoredLocalStorage();
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 await builder.Build().RunAsync();
